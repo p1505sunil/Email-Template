7ba031a34987f3e30aadf1fa8e3a9f77ca6dcf4e
p1505sunil <52889283+p1505sunil@users.noreply.github.com>
2019-08-17 08:23:55 -0500

    Add project files.

---
+++ Email-Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Email_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
