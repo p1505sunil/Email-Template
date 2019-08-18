c71241e6ccacac704f975d4ae2d282c5e26aecad
sunil <sunil.papineni1505@gmail.com>
2019-08-18 16:35:05 -0400

    Implemented logic for sending messgae

---
+++ Email-Template/Services/MessageServices.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MailKit.Security;
+
+namespace DemoApp.Services
+{
+    public class AuthMessageSender : IEmailSender, ISmsSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            try
+            {
+                //From Address  
+                string FromAddress = "myname@company.com";
+                string FromAdressTitle = "ASP.NET Core DemoApp";
+                //To Address  
+                string ToAddress = email;
+                string ToAdressTitle = "Nishan Aryal";
+                string Subject = subject;
+                string BodyContent = message;
+
+                //Smtp Server  
+                string SmtpServer = "smtp.office365.com";
+                //Smtp Port Number  
+                int SmtpPortNumber = 587;
+
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress
+                                        (FromAdressTitle, 
+                                         FromAddress
+                                         ));
+                mimeMessage.To.Add(new MailboxAddress
+                                         (ToAdressTitle,
+                                         ToAddress
+                                         ));
+                mimeMessage.Subject = Subject; //Subject
+                mimeMessage.Body = new TextPart("html")
+                {
+                    Text = BodyContent
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect(SmtpServer, SmtpPortNumber, false);
+                    client.Authenticate(
+                        "myemail@mydomain.com",  //Enter your email here
+                        "MyPassword" //Enter your Password here.
+                        );
+                   await client.SendAsync(mimeMessage);
+                    Console.WriteLine("The mail has been sent successfully !!");
+                    Console.ReadLine();
+                   await client.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public Task SendSmsAsync(string number, string message)
+        {
+            // Plug in your SMS service here to send a text message.
+            return Task.FromResult(0);
+        }
+    }
+}
